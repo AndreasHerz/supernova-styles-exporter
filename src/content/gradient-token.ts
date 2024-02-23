307b01411c9af069171328268149ab768f8d7e2f
Andreas Herz <andreas.herz@sevdesk.de>
2024-02-23 13:12:54 +0100

    chore: generate colors & gradients

---
+++ src/content/gradient-token.ts
@@ -0,0 +1,36 @@
+import { NamingHelper, CSSHelper, ColorFormat, StringCase } from '@supernovaio/export-helpers';
+import { ColorToken, GradientToken, Token, TokenGroup } from '@supernovaio/sdk-exporters';
+
+export function createGradients(
+  tokens: GradientToken[],
+  mappedTokens: Map<string, Token>,
+  tokenGroups: Array<TokenGroup>
+): string {
+  return tokens.map((token) => gradientTokenToSCSS(token as GradientToken, mappedTokens, tokenGroups)).join('\n');
+}
+
+function gradientTokenToSCSS(
+  token: GradientToken,
+  mappedTokens: Map<string, Token>,
+  tokenGroups: Array<TokenGroup>
+): string {
+  // First creating the name of the token, using helper function which turns any token name / path into a valid variable name
+  const name = tokenVariableName(token, tokenGroups);
+
+  // Then creating the value of the token, using another helper function
+  const value = CSSHelper.gradientTokenValueToCSS(token.value, mappedTokens, {
+    allowReferences: true,
+    decimals: 1,
+    colorFormat: ColorFormat.smartHashHex,
+    tokenToVariableRef: (t) => {
+      return '$' + tokenVariableName(t, tokenGroups);
+    },
+  });
+
+  return `$${name}: ${value};`;
+}
+
+function tokenVariableName(token: Token, tokenGroups: Array<TokenGroup>): string {
+  const parent = tokenGroups.find((group) => group.id === token.parentGroupId)!;
+  return NamingHelper.codeSafeVariableNameForToken(token, StringCase.paramCase, parent, 'gradient');
+}
